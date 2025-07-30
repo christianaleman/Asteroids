cbab8c155540bdd84f0cab69ee06b3c23600efd8
Christian Aleman <christian.aleman@gmail.com>
2025-07-30 22:28:26 +0200

    fps to main.py and added circleshape class

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
